6faf0a85380db673eaa8fee4232eacb365175c99
alfred <alfred@nurd.nu>
2017-02-08 22:19:35 +0000

    dont count zookeeper when counting error in the end of integration test

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -2,7 +2,8 @@
 pushd "$(dirname "$0")" >/dev/null
 docker-compose up --abort-on-container-exit --build --force-recreate
 exitstatus="$(docker-compose ps 2>&1)"
-unsuccessful="$(echo "$exitstatus" | grep "Exit [1-9]" | wc -l)"
+# Zookeeper always(?) exits with an error, so don't include it when counting errors.
+unsuccessful="$(echo "$exitstatus" | grep -v zookeeper | grep "Exit [1-9]" | wc -l)"
 popd >/dev/null
 if [ "$unsuccessful" != "0" ]; then
   echo -e "\n$unsuccessful container(s) exited with an error code:\n"
